635ba4d39220aa6a0d70614a22e7bba2672d7407
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-04-09 09:13:40 -0300

    Using vec:unpack

--- game/main.lua
+++ game/main.lua
@@ -15,7 +15,7 @@ function love.draw ()
   local graphics = love.graphics
   for _,element in ipairs(elements) do
     graphics.push()
-    graphics.translate(element.pos.x, element.pos.y)
+    graphics.translate(element.pos:unpack())
     element:draw(graphics)
     graphics.pop()
   end
