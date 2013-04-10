5e63567bd0c1868211e3446edb4176e82dbd4ffc
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-04-09 22:23:42 -0300

    Loads scenes with simple elements

--- game/scene.lua
+++ game/scene.lua
@@ -3,23 +3,26 @@ module ('scene', package.seeall)
 
 require 'lux.functional'
 require 'lux.geom.vector'
+require 'base.element'
 
 local lambda        = lux.functional
 local scenefile_env = {}
 
 local function get_element_type (typename)
-  
+  return base.element
 end
 
 local function new_element (elements, name, info)
   info.type = info.type or 'element'
   info.data.name = name
-  table.insert(elements, get_element_type(info.type):new(info.data))
+  local new_elem = get_element_type(info.type):new(info.data)
+  table.insert(elements, new_elem)
 end
 
 local function prepare_env (env, elements)
-  env.element = lambda.chain((lambda.bindleft(new_element, env)), 1)
-  env.point   = lambda.bindleft(lux.geom.vector.new, lux.geom.vector)
+  env.element = lambda.chain((lambda.bindleft(new_element, elements)), 1)
+  env.point   = lambda.bindleft(lux.geom.point.new, lux.geom.point)
+  env.print   = print
 end
 
 function load (file)
