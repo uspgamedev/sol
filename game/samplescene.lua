13148b608961c0e75f9d881e3357193684723052
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-04-10 19:04:34 -0300

    More interesting sample scene.

--- game/samplescene.lua
+++ game/samplescene.lua
@@ -1,7 +1,12 @@
 
-element 'Name'
+element 'This is grabbable'
   :add_property 'visible' {
     pos = point{400,300}
   }
   :add_property 'grabbable' {}
 
+element 'This is NOT grabbable'
+  :add_property 'visible' {
+    pos = point{200,200},
+    size = vector{256,32}
+  }
