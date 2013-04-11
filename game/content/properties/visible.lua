440619d8efaf56f177cdc18c8a39a7a3d867878c
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-04-11 11:42:13 -0300

    Fancy error message qhen loading function from bad string

--- game/content/properties/visible.lua
+++ game/content/properties/visible.lua
@@ -13,7 +13,7 @@ visible = base.property:new {
 function visible.triggers:draw (graphics)
   local draw = self.visible.draw
   if type(draw) == 'string' then
-    draw = loadstring(draw)
+    draw = assert(loadstring(draw), "Element '"..self.name.."': Couldn't load draw function.")
   end
   draw(self, graphics)
 end
