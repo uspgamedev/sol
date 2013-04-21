d9c6ea3933819a2a7a84c284a9d75b13700a3dd0
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-04-21 00:45:25 -0300

    Triggers are created dynamically now

--- game/content/triggers.lua
+++ game/content/triggers.lua
@@ -1,6 +1,17 @@
 
 module ('content.triggers', package.seeall)
 
+local module_mttab = getmetatable(_M)
+
+function module_mttab:__call (name)
+  if self[name] then
+    return self[name]
+  end
+  local new_trigger = base.trigger:new {}
+  self[name] = new_trigger
+  return new_trigger
+end
+
 local all_triggers = love.filesystem.enumerate 'content/triggers'
 for _, trigger_file in ipairs(all_triggers) do
   require('content.triggers.'..string.gsub(trigger_file, '.lua', ''))
