e29736ff4193d226c809dd5a8e8b4ef5d531ab8a
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-04-24 22:23:30 -0300

    Renamed 'times' to 'counts_time'

---
+++ game/content/properties/counts_time.lua
@@ -0,0 +1,31 @@
+module ('content.properties', package.seeall)
+
+require 'base.property'
+
+counts_time = base.property:new {
+  limit     = 1.0,    -- in seconds
+  counter   = 0.0,    -- counts time, shouldn't be changed
+  repeats   = false,
+  totrigger = 'never'
+}
+
+function counts_time:setup( element )
+  content.triggers(self.totrigger)
+end
+
+function counts_time.triggers:update (dt)
+  self.counts_time.counter = self.counts_time.counter + dt
+  if self.counts_time.counter >= self.counts_time.limit then
+    content.triggers(self.counts_time.totrigger):activate()
+    self.counts_time.counter = self.counts_time.counter - self.counts_time.limit
+    if not self.counts_time.repeats then
+      self:destroy()
+    end
+  end
+end
+
+--make.timer 'Timer' {
+--  timetotrigger = 2,
+--  repeats = true,
+--  totrigger = 'timer'
+--}
