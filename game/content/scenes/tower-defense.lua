764b732038598ee1a27f33a8e9cdf216dc711f56
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-04-26 19:38:45 -0300

    Tower defense?

--- game/content/scenes/tower-defense.lua
+++ game/content/scenes/tower-defense.lua
@@ -1,11 +1,4 @@
---make.creator 'shooter-placer' {
---  recipe  = 'creator',
---  trigger = 'mouse_pressedleft',
---  args = {
---    recipe = 'solidbody',
---
---  }
---}
+local unique_death = unique'death'
 
 local function gambiarra (element)
   element
@@ -15,29 +8,62 @@ local function gambiarra (element)
         fromcontext = '',
         to = 'life',
         when = 'collision',
-        value = [[print'wat' or property.life-1]]
+        value = [[property.life-1]]
       },
       apply {
         fromcontext = '',
         to = 'life',
         ifcondition = 'property.life <= 0',
-        also_trigger = 'death',
+        also_trigger = unique_death(element),
         value = '0'
       }
     }
   return 'collision'
 end
 
+local function get_deathtrigger (element)
+  return element.collides[2].specs.also_trigger
+end
+
+element 'Player'
+  :property 'counts_time'{
+    repeats = true,
+    apply {
+      fromcontext='money',
+      to='counter',
+      when='mouse_pressedleft',
+      value='property.counter',
+      ifcondition='@value > 0',
+      also_trigger='create'
+    },
+  }
+
+make.score 'money' {
+  initial_value = 10,
+  position      = point{100,100},
+  fontsize      = 18,
+  change_when   = 'create',
+  change        = [[element.visible.score-1]]
+}
+element 'money'
+  :property 'visible' {
+    share {
+      incontext='money',
+      valueof='value',
+      as=[[property.score]]
+    }
+  }
+
 make.creator 'turret-placer' {
   recipe  = 'solidbody',
-  when    = 'mouse_pressedleft',
+  when    = 'create',
   args    = {
     visual = sprite{ mirror = {true, false} },
     apply { fromcontext='mouse', to='position', value='@position'},
     collision_trigger     = gambiarra,
     collision_class       = 'turret',
     collision_targetclass = 'enemy',
-    destroyed_when        = 'death'
+    destroyed_when        = get_deathtrigger
   }
 }
 
@@ -51,12 +77,18 @@ make.creator 'enemy-shooter' {
   args    = {
     position = start_position,
     visual = rectangle { width = 10, height = 10, color = {100,100,100} },
-    initial_speed = vector{-100,0},
+    initial_speed         = vector{-100,0},
     collision_class       = 'enemy',
     collision_targetclass = 'turret',
     collision_trigger     = 'collision',
     destroyed_when        = 'collision'
   }
+}
+
+make.solidbody 'wall' {
+  position          = point{10,screen.height/2},
+  visual            = rectangle{ width=10, height=screen.height},
+  collision_class   = 'turret',
 }
 
 make.timer 'pinger' {
