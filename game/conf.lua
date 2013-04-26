b23e75b102e26f0c822bd1865595737216e1d26f
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-04-26 18:48:06 -0300

    Tower defense start!

--- game/conf.lua
+++ game/conf.lua
@@ -13,8 +13,8 @@ function love.conf(t)
     t.version = "0.8.0"
     t.console = false
     t.release = false
-    t.screen.width = 1024
-    t.screen.height = 768
+    t.screen.width = 800
+    t.screen.height = 600
     t.screen.fullscreen = false
     t.screen.vsync = true
     t.screen.fsaa = 0
---
+++ game/content/scenes/tower-defense.lua
@@ -0,0 +1,74 @@
+--make.creator 'shooter-placer' {
+--  recipe  = 'creator',
+--  trigger = 'mouse_pressedleft',
+--  args = {
+--    recipe = 'solidbody',
+--
+--  }
+--}
+
+local function gambiarra (element)
+  element
+    :property'collides'{
+      life = 3,
+      apply {
+        fromcontext = '',
+        to = 'life',
+        when = 'collision',
+        value = [[property.life-1]]
+      },
+      apply {
+        fromcontext = '',
+        to = 'life',
+        ifcondition = 'property.life <= 0',
+        also_trigger = 'death',
+        value = '0'
+      }
+    }
+  return 'collision'
+end
+
+make.creator 'turret-placer' {
+  recipe  = 'solidbody',
+  when    = 'mouse_pressedleft',
+  args    = {
+    visual = sprite{ mirror = {true, false} },
+    apply { fromcontext='mouse', to='position', value='@position'},
+    collision_trigger     = gambiarra,
+    collision_class       = 'turret',
+    collision_targetclass = 'enemy',
+    destroyed_when        = 'death'
+  }
+}
+
+local function start_position ()
+  return point{screen.width+100, random()*screen.height}
+end
+
+make.creator 'enemy-shooter' {
+  recipe  = 'solidbody',
+  when    = 'ping',
+  args    = {
+    position = start_position,
+    visual = rectangle { width = 10, height = 10, color = {100,100,100} },
+    initial_speed = vector{-100,0},
+    collision_class       = 'enemy',
+    collision_targetclass = 'turret',
+    collision_trigger     = 'collision',
+    destroyed_when        = 'collision'
+  }
+}
+
+make.timer 'pinger' {
+  limit     = 3.0,
+  repeats   = true,
+  totrigger = 'ping'
+}
+element 'pinger': property 'counts_time' {
+  apply {
+    fromcontext = '',
+    to          = 'limit',
+    value       = [[property.limit*0.9]],
+    when        = 'ping'
+  }
+}
